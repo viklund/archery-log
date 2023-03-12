52d661aa3d8fe05d1319b1a66086ef5957221af3
Johan Viklund <johan.viklund@nbis.se>
2023-03-12 23:06:25 +0100

    on('hit') callback for the target
    The "event" has information about where the hit was including references
    to the svg elements.

--- js.js
+++ js.js
@@ -1,22 +1,39 @@
 class Target {
     constructor(size=350, zoom=6, canvas='div#canvas') {
-        this.SIZE   = size;
-        this.WIDTH  = this.SIZE * 5/4;
-        this.CENTER = this.WIDTH/2;
-        this.RADIUS = this.SIZE/2;
-        this.ZOOM   = zoom;
-        this.ARROW  = 1;
-        this.CANVAS = canvas;
+        this.SIZE      = size;
+        this.WIDTH     = this.SIZE * 5/4;
+        this.CENTER    = this.WIDTH/2;
+        this.RADIUS    = this.SIZE/2;
+        this.ZOOM      = zoom;
+        this.ARROW     = 1;
+        this.CANVAS    = canvas;
+        this.callbacks = {};
+    }
+
+    on(event, callback) {
+        if (event != 'hit') {
+            console.log("Can't handle event " + event);
+            return;
+        }
+        if (! (event in this.callbacks) ) {
+            this.callbacks[event] = [];
+        }
+        this['callbacks'][event].push( callback );
+    }
+
+    run_callback(event, argument) {
+        if (event in this.callbacks) {
+            for (let cb of this.callbacks[event]) {
+                cb(argument);
+            }
+        }
     }
 
     // Just a setup function
     paintTargets() {
-        console.log(this.CANVAS);
         var svg = SVG().addTo(this.CANVAS).size(this.WIDTH,this.WIDTH);
         var draw = svg.group().transform({scale: 1});
-        var rect = draw.rect(this.WIDTH, this.WIDTH).attr({ fill: "#f46" });
-
-        console.log(svg);
+        draw.rect(this.WIDTH, this.WIDTH).attr({ fill: "#f46" });
 
         this['target'] = {}
         this['target']['node'] = svg.node;
@@ -98,7 +115,6 @@ class Target {
     onClick(e) {
         e.preventDefault();
         e.stopPropagation();
-        //var off = e.target.getBoundingClientRect();
         var [x,y] = this.eventCoordinate(e);
 
         var bx = (x-this.CENTER)*this.ZOOM + this.CENTER;
@@ -106,49 +122,55 @@ class Target {
 
         var hit = {};
 
-        var r = this.target.group.circle(10).fill('white').stroke({color:'black',width:2}).attr({cx:x,cy:y});
-        this.zoomed.group.circle(10*this.ZOOM/2).fill('white').stroke({color:'black',width:2}).attr({cx:bx,cy:by});
+        hit.arrow = this.ARROW++;
+
+        hit['svg_target'] = this.target.group.circle(10).fill('white').stroke({color:'black',width:2}).attr({cx:x,cy:y});
+        hit['svg_zoomed'] = this.zoomed.group.circle(10*this.ZOOM/2).fill('white').stroke({color:'black',width:2}).attr({cx:bx,cy:by});
+
+        hit.x = 10*(x-this.CENTER)/this.RADIUS;
+        hit.y = 10*(this.CENTER-y)/this.RADIUS;
+
+        var angle = Math.atan(hit.y/hit.x)*180/Math.PI;
+        if ( hit.x < 0 ) {
+            angle += 180;
+        }
+        else if (hit.y < 0 ) {
+            angle += 360;
+        }
+        hit.angle = angle;
 
         var dist = Math.sqrt( (this.CENTER - x)**2 + (this.CENTER - y)**2);
-        var size_of_one_segment = this.SIZE/20;
-        var score = 10 - Math.floor(dist/size_of_one_segment);
+        var size_of_one_segment = this.RADIUS/10;
+        hit.dist = dist/size_of_one_segment;
+
+        var score = 11 - dist/size_of_one_segment;
         if (score <= 0) {
             score = 0;
         }
-        var tbl = document.getElementById('table');
-        var row = document.createElement('tr');
-        var c1  = document.createElement('td');
-        c1.innerText = this.ARROW++;
-        row.appendChild(c1);
-        var c1  = document.createElement('td');
-        c1.innerText = score;
-        row.appendChild(c1);
-        var c2 = document.createElement('td');
-        c2.innerText = Math.floor( 100*(x-this.CENTER)/this.RADIUS );
-        row.appendChild(c2);
-        var c3 = document.createElement('td');
-        c3.innerText = Math.floor( 100*(y-this.CENTER)/this.RADIUS );
-        row.appendChild(c3);
-        tbl.appendChild(row);
+        score = Math.floor(score);
+
+        hit.score = score;
+
+        this.run_callback('hit', hit);
     }
 
     _paintTarget(s, radius, cx, cy) {
-        var cs = {cx:cx, cy:cy};
-        var size = radius*2;
+        let cs = {cx:cx, cy:cy};
+        let size = radius*2;
 
-        var COLORS = ["#ccc", "#000", "#00f", "#f00", "#ff0"];
-        for (var i=0 ; i<5; i++) {
-            var r = size - i*size/5;
+        let COLORS = ["#ccc", "#000", "#00f", "#f00", "#ff0"];
+        for (let i=0 ; i<5; i++) {
+            let r = size - i*size/5;
             s.circle(r).attr({fill: COLORS[i]}).attr(cs);
         }
 
         s.circle(size/16).fill('none').stroke({color:"#000"}).attr(cs);
-        for (var i=0 ; i<10; i++) {
-            var color = "#000";
+        for (let i=0 ; i<10; i++) {
+            let color = "#000";
             if (i>1 && i<5) {
                 color = "#fff";
             }
-            var r = size - i*size/10;
+            let r = size - i*size/10;
             s.circle(r).fill('none').stroke({color:color}).attr(cs);
         }
     }
@@ -156,3 +178,22 @@ class Target {
 
 var t = new Target();
 t.paintTargets();
+t.on('hit', (h) => {
+    console.log(h);
+
+    var tbl = document.getElementById('table');
+    var row = document.createElement('tr');
+    var c1  = document.createElement('td');
+    c1.innerText = h.arrow;
+    row.appendChild(c1);
+    c1 = document.createElement('td');
+    c1.innerText = h.score;
+    row.appendChild(c1);
+    var c2 = document.createElement('td');
+    c2.innerText = Math.floor( h.x );
+    row.appendChild(c2);
+    var c3 = document.createElement('td');
+    c3.innerText = Math.floor( h.y );
+    row.appendChild(c3);
+    tbl.appendChild(row);
+});
