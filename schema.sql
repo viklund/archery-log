8a2784f5d08a08d605763a72a1011d9b988eef97
Johan Viklund <johan.viklund@nbis.se>
2023-03-16 23:06:03 +0100

    Added session table

--- schema.sql
+++ schema.sql
@@ -1,8 +1,15 @@
 CREATE TABLE hits (
     id integer primary key autoincrement not null,
+    session integer references sessions(id),
     x real,
     y real,
     angle real,
     dist real,
     score int
+);
+
+CREATE TABLE sessions (
+    id integer primary key autoincrement not null,
+    starttime integer default ( datetime() ),
+    open boolean default false
 );
